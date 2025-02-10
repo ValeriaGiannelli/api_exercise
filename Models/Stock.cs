898ded60db2bfa93c0482372e3fdb26f4288c7a1
vale.giannelli <vg.giannellivaleria@gmail.com>
2025-02-10 11:03:13 +0100

    fix: add data annotation

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 /// <summary>
 /// Summary description for Class1
@@ -7,23 +9,23 @@ namespace api.Models
 {
 	public class Stock
 	{
-		public int Id { get; set; }
+        public int Id { get; set; }
 
-		public string Symbol { get; set; } = string.Empty; //simbolo
+        public string Symbol { get; set; } = string.Empty; //simbolo
 
         public string CompanyName { get; set; } = string.Empty; //nome dell'azienda
 
         [Column(TypeName = "decimal(18,2)")]
-		public decimal Purchase {  get; set; } //costo
+        public decimal Purchase { get; set; } //costo
 
         [Column(TypeName = "decimal(18,2)")]
         public decimal LastDiv { get; set; } //porzione di utile della società
 
         public string Industry { get; set; } = string.Empty; //nome dell'industria
 
-        public long MarketCap{ get; set; }; //valore totale azioni
+        public long MarketCap { get; set; } //valore totale azioni
 
-        public List<Comment> Comments { get; set; } = new List<Comment>; //commenti linkati con classe comment
+        public List<Comment> Comments { get; set; } = new List<Comment>(); //commenti linkati con classe comment
 
     }
 }
