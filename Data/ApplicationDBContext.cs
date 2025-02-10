a2ebc0fb2c24e170891aecc21ed1f2efa2f6cb27
vale.giannelli <vg.giannellivaleria@gmail.com>
2025-02-10 11:02:27 +0100

    feat: create DB class to define tables, configure db and execute query

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+
+namespace api.Data
+{
+	public class ApplicationDBContext : DbContext
+	{
+		public ApplicationDBContext(DbContextOptions<ApplicationDBContext> dbContextOption)
+			:base(dbContextOption)
+		{
+			
+		}
+
+		//creazione tabelle
+		public DbSet<Stock> Stock { get; set; } 
+		public DbSet<Comment> Comment { get; set; }
+	}
+}
