8fbc69c1a83f1508b04efe8bc4d6da8ccd029ceb
vale.giannelli <vg.giannellivaleria@gmail.com>
2025-02-10 09:30:00 +0100

    feat: create class stock and comment

---
+++ Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace api.Models
+{
+	public class Comment
+	{
+		public int Id { get; set; }
+		public string Title { get; set; } = string.Empty; //titolo
+		public string Content { get; set; } = string.Empty; //contenuto
+        public DateTime CreateOn { get; set; } = DateTime.Now; //data di creazione default ora
+
+        public int? StockId { get; set; } //FK
+
+        //proprietà di navigazione
+        public Stock? Stock { get; set; }
+	}
+}
