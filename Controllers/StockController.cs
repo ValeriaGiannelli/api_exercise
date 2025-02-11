ecce352f77a2d0b0067aacaf846a5672b2a8a5b3
vale.giannelli <vg.giannellivaleria@gmail.com>
2025-02-11 12:43:39 +0100

    feat: add delete function

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -97,5 +97,28 @@ namespace api.Controllers
 
             return Ok(stockModel.ToStockDto());
         }
+
+        /// <summary>
+        /// Metodo per cancellare l'elemento
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+
+            return NoContent();
+
+        }
     }
 }
