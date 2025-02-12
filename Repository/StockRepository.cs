a8a09cfd02515ace8ad9ef31a92b785a11b8afc4
vale.giannelli <vg.giannellivaleria@gmail.com>
2025-02-12 10:06:28 +0100

    feat: create interface and Repository

---
+++ Repository/StockRepository.cs
@@ -0,0 +1,23 @@
+﻿using api.Data;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class StockRepository : IStockRepository
+    {
+        //injection: bisogna chiamare il DB
+        private readonly ApplicationDBContext _context;
+
+        //crea il costruttore
+        public StockRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        Task<List<Stock>> IStockRepository.GetAllAsync()
+        {
+            return _context.Stocks.ToListAsync();
+        }
+    }
+}
